d7f0ad815a41b76d7791070af5c58a7ae267e60c
ewan <ewan@ubuntu.myguest.virtualbox.org>
2023-09-12 16:10:25 +0100

    displays message when player wins

--- frontend/src/Components/ChessRoom.tsx
+++ frontend/src/Components/ChessRoom.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useState } from "react";
+import React, { ChangeEvent, useEffect, useState } from "react";
 import useWebSocket, { ReadyState }  from "react-use-websocket";
 import { Chess } from "chess.js"
 import { Chessboard } from "react-chessboard"
@@ -16,6 +16,8 @@ const ChessRoom = ({roomName, leaveRoom}:Props) => {
 	const [game, setGame] = useState<Chess>(new Chess());
 	const [turn, setTurn] = useState<boolean>(false);
 	const [gameStart, setGameStart] = useState<boolean>(false);
+	const [gameOver, setGameOver] = useState<boolean>(false);
+	const [overMessage, setOverMessage] = useState<string>("");
 
 	const room_slug = roomName.replaceAll(' ', '-');
 	const {sendJsonMessage, readyState} = useWebSocket('ws://'
@@ -72,7 +74,16 @@ const ChessRoom = ({roomName, leaveRoom}:Props) => {
 	}
 
 	const sendChessMove = (currentMove:{from:string, to:string, promotion:string}) => {
-		sendJsonMessage({"type": "chess_move", "move": currentMove});	
+		sendJsonMessage({"type": "chess_move", "move": currentMove});
+		
+	}
+	useEffect(() => {
+		if(game.isGameOver()) handleGameEnd();
+	}, [game])
+	const handleGameEnd = () => {
+		setGameOver(true);
+		if (game.isCheckmate()) setOverMessage(`${game.turn() === 'w' ? 'Black': 'White'} wins by checkmate.`);
+		if (game.isDraw()) setOverMessage('Draw');
 	}
 
 	const makeChessMove = (currentMove:{from:string, to:string, promotion:string}) => {
@@ -80,6 +91,7 @@ const ChessRoom = ({roomName, leaveRoom}:Props) => {
 		gameCopy.loadPgn(game.pgn());
 		const move = gameCopy.move(currentMove);
 		setGame(gameCopy);
+
 		return move;
 	}
 
@@ -101,24 +113,23 @@ const ChessRoom = ({roomName, leaveRoom}:Props) => {
 			<input type="text" name="chatMessage" value={message} onChange={changeMessage} />
 			<p>Last message: {lastMessage}</p>
 			<button onClick={handleSendMessage}>Send message</button>
-			{ gameStart 
-				? (	
+
+			{ !gameStart &&
+				<p> Game has not started </p>
+			}
+
+			{ (gameStart && !gameOver) &&
 				<>
-				{ turn 
-					? (
-						<p>It is your turn</p>
-					) : (
-					<p>Opponent's turn</p>
-				)
+				{ turn
+					? <p>It is your turn</p>
+					: <p>Opponent's turn</p>
 				}
-				</> 
-				) 
-				: (
-					<p>Game has not started.</p>
-				)}
+				</>
+			}
 
-
-
+			{ gameOver &&
+				<p> {overMessage} </p>
+			}
 
 			<Chessboard position={game.fen()} onPieceDrop={onDrop} />
 			<button onClick={() => {leaveRoom();}}>Leave</button>
